b42a4ac9f815092173f3775e67ebdb1bdc5155c8
Jordan Carney <jordan@jordancarney.com>
2016-10-07 20:02:46 -0400

    Tweaked for heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var moment = require('moment')
 var app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.send('Welcome!!')
 })
@@ -28,4 +30,6 @@ app.get('/:date', function (req, res) {
   res.send(timeStamp);
 })
 
-app.listen(process.env.PORT || <default port>)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
