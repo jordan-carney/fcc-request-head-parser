7b8e6121ae712e34a7ebc067dfac36c867fcc473
Jordan Carney <jordan@jordancarney.com>
2016-12-14 20:58:06 -0500

    Removed uneeded files. Updated IP address reporting.

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express()
 app.set('port', (process.env.PORT || 5000))
 
 app.use('/', function (req, res) {
+  let ipType = 'IPv4'
+  if ( req.ip.slice(0, 7) === "::ffff:" ) ipType = 'IPv6'
+
   const reqData = {
-    ipaddress: req.ip,
-    software: req.headers['user-agent'],
+    ipaddress: req.ip + ' ' + ipType,
     language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
   }
   res.send(reqData)
 })
